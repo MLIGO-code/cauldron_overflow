0622364abbd0862216c821948f8436c5f85f0b05
Dominik Wołos <dominik.wolos@gmail.com>
2021-07-23 13:34:31 +0200

    we are rocking this symfony thing

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,7 +4,28 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('What a bewiching controller we have conjured! ');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+
+    public function show($slug){
+        return new Response(sprintf(
+            'I will put there picture of my foot after "%s"!',
+            str_replace('-',' ',$slug)
+        ));
+    }
 
 }
