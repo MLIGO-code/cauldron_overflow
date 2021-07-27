f1e74fd2a7709d7b29d7b09c517ca5f650104fc3
Dominik Wołos <dominik.wolos@gmail.com>
2021-07-27 15:19:56 +0200

    Fixtures

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 
 
 use App\Entity\Question;
+use App\Repository\QuestionRepository;
 use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -32,6 +34,7 @@ class QuestionController extends AbstractController
         $questionDetails = new Question();
         $questionDetails->setName("Missing Pants")
             ->setSlug('missing-pants-'.rand(0,1000))
+            ->setVotes(rand(-20,50))
             ->setQuestion(<<<EOF
 Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
 to make my dishes wash themselves. But while I was casting it,
@@ -52,46 +55,71 @@ EOF
         ));
     }
 
+
+
+
+
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(QuestionRepository $repository)
     {
-        /*fancy way of using Twig service
-        $html=$twigEnvironment->render('question/homepage.html.twig');
-        return new Response($html);
-        */
+       $questions = $repository->findAllAskedOrderedByNewest();
 
-        return $this->render('question/homepage.html.twig');
+        return $this->render('question/homepage.html.twig',[
+            'questions'=>$questions,
+        ]);
     }
+
+
+
+
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper ,
-                        HubInterface $sentryHub){
+    public function show(Question $question){
 
         if($this->isDebug)
             $this->debugger->info("We are in debug mode");
 
-        $questionText='I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $answers=[
+            'Make sure your cat is sitting `purrfectly` still xD',
+            'Honestly , I like furry shoes better than MY cat',
+            'Maybe ... you try saying the spell backwards???'
+        ];
 
-       $answers=[
-           'Make sure your cat is sitting `purrfectly` still xD',
-           'Honestly , I like furry shoes better than MY cat',
-           'Maybe ... you try saying the spell backwards???'
-       ];
-
-        $parsedQuestionText=$markdownHelper->parse($questionText);
-
+        //dump($question);
         return $this->render('question/show.html.twig',[
-            'question' => ucwords(str_replace('-',' ',$slug)),
-            'questionText' => $parsedQuestionText,
+            'question' => $question,
             'answers' => $answers,
         ]);
 
     }
 
+    /**
+     * @Route ("/question/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request,
+                EntityManagerInterface $entityManager,
+    )
+    {
+        $direction=$request->request->get('direction');
+        if($direction==='up')
+            $question->upVote();
+        elseif($direction==='down')
+            $question->downVote();
 
+        $entityManager->flush();
 
+        $answers=[
+            'Make sure your cat is sitting `purrfectly` still xD',
+            'Honestly , I like furry shoes better than MY cat',
+            'Maybe ... you try saying the spell backwards???'
+        ];
+
+        return $this->redirectToRoute('app_question_show',[
+            'slug'=>$question->getSlug()
+        ]);
+    }
 }
