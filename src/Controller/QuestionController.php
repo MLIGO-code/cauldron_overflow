d13a2bfdc3707a12a88a5ea98533e998ad85ba05
Dominik Wołos <dominik.wolos@gmail.com>
2021-07-27 11:16:33 +0200

    doctrine database first migrations

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,7 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Question;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,6 +24,33 @@ class QuestionController extends AbstractController
         $this->debugger = $debugger;
         $this->isDebug = $isDebug;
     }
+    /**
+     * @Route ("/questions/new-question")
+     */
+    public function newQuestion(EntityManagerInterface $entityManager)
+    {
+        $questionDetails = new Question();
+        $questionDetails->setName("Missing Pants")
+            ->setSlug('missing-pants-'.rand(0,1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+        if(rand(0,10)>2) $questionDetails
+            ->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1,100))));
+        $entityManager ->persist($questionDetails);
+        $entityManager -> flush();
+        return new Response(sprintf('Well hello , the shiny new question is id #%d, slug %s',
+        $questionDetails->getId(),
+        $questionDetails->getSlug(),
+        ));
+    }
 
     /**
      * @Route("/", name="app_homepage")
@@ -31,6 +61,7 @@ class QuestionController extends AbstractController
         $html=$twigEnvironment->render('question/homepage.html.twig');
         return new Response($html);
         */
+
         return $this->render('question/homepage.html.twig');
     }
 
@@ -60,5 +91,7 @@ class QuestionController extends AbstractController
         ]);
 
     }
+
+
 
 }
